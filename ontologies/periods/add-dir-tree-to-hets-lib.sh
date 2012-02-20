e608bb93164eeea7709fc4e250d8b663ea3a7dcd
allegristas@gmail.com <allegristas@gmail.com@fbc90b2e-2927-0945-06b8-092a73861d4d>
2012-02-20 11:55:41 +0000

    sync before move

--- ontologies/periods/add-dir-tree-to-hets-lib.sh
+++ ontologies/periods/add-dir-tree-to-hets-lib.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+# 
+# Prepends the current directory and all of its non-hidden subdirectories to the HETS_LIB path
+# 
+# Run with "source add-dir-tree-to-hets-lib.sh" in your shell.
+#
+# This is a temporary workaround until the resolution of relative (cl-imports) works.
+#
+# Christoph Lange, 2012-02-20
+
 find $PWD -depth -type d ! -path '*/.*' -print | while read dir
 do
-    echo $dir
+    HETS_LIB="$dir:$HETS_LIB"
 done
+export HETS_LIB
